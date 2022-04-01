c2bcd3a5699d85723c09325798e5598277f86ca2
James L <59751464+Jazz1016@users.noreply.github.com>
2022-04-01 12:33:42 -0600

    started on chapter 2

---
+++ LifeCycle/LifeCycle/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  LifeCycle
+//
+//  Created by James Lea on 4/1/22.
+//
+
+import Foundation
+
+class MyClass {
+    func methodOne() {
+        print(">> methodOne")
+    }
+    
+    func methodTwo() {
+        print(">> methodTwo")
+    }
+}
---
+++ LifeCycle/LifeCycleTests/MyClassTests.swift
@@ -0,0 +1,49 @@
+//
+//  MyClassTests.swift
+//  LifeCycleTests
+//
+//  Created by James Lea on 4/1/22.
+//
+
+import XCTest
+@testable import LifeCycle
+
+class MyClassTests: XCTestCase {
+    
+    func test_zero(){
+        XCTFail("Tests not yet implemented in MyClassTests")
+    }
+
+    func test_methodOne() {
+        let sut = MyClass()
+        
+        sut.methodOne()
+    }
+    
+    func test_methodTwo(){
+        let sut = MyClass()
+        
+        sut.methodTwo()
+    }
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
