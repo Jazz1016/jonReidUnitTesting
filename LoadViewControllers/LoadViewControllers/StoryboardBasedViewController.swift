50d12fd615a2769a76d1ef2e056077394ebf7622
James L <59751464+Jazz1016@users.noreply.github.com>
2022-04-13 12:18:29 -0600

    completed storyboard based view controller testing example

---
+++ LoadViewControllers/LoadViewControllers/StoryboardBasedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StoryboardBasedViewController.swift
+//  LoadViewControllers
+//
+//  Created by James Lea on 4/13/22.
+//
+
+import UIKit
+
+class StoryboardBasedViewController: UIViewController {
+    
+    // MARK: Outlets -
+    @IBOutlet weak var label: UILabel!
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ LoadViewControllers/LoadViewControllersTests/StoryboardBasedViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  StoryboardBasedViewControllerTests.swift
+//  LoadViewControllersTests
+//
+//  Created by James Lea on 4/13/22.
+//
+
+import XCTest
+import UIKit
+@testable import LoadViewControllers
+
+class StoryboardBasedViewControllerTests: XCTestCase {
+    
+    func test_zero(){
+        
+    }
+    
+    func test_loading(){
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let sut: StoryboardBasedViewController = sb.instantiateViewController(identifier: String(describing: StoryboardBasedViewController.self))
+        sut.loadViewIfNeeded()
+        XCTAssertNotNil(sut.label)
+    }
+
+}
