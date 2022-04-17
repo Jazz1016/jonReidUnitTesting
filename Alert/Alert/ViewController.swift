90b944146c374fa68f298a80dbd0369faa682db9
James L <59751464+Jazz1016@users.noreply.github.com>
2022-04-17 13:12:48 -0600

    having trouble adding viewcontroller spy framework

--- Alert/Alert/ViewController.swift
+++ Alert/Alert/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private(set) weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction private func buttonTap(_ sender: Any) {
+        let alert = UIAlertController(
+            title: "Do the Thing?",
+            message: "Let us know wif you want to do the thing",
+            preferredStyle: .alert)
+        let cancelAction = UIAlertAction(
+            title: "Cancel",
+            style: .default) { _ in
+                print("cancel")
+            }
+        let okAction = UIAlertAction(
+            title: "Ok",
+            style: .default) { _ in
+            print(">> Ok")
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        alert.preferredAction = okAction
+        present(alert, animated: true)
+    }
+    
+    
 
 }
 
---
+++ Alert/AlertTests/ViewControlllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControlllerTests.swift
+//  AlertTests
+//
+//  Created by James Lea on 4/17/22.
+//
+
+import XCTest
+//import ViewControllerPresentationSpy
+
+class ViewControlllerTests: XCTestCase {
+    
+    
+    
+}
