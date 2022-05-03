1e098e30c243956fdccf1062ba8afb47c54dc14f
James L <59751464+Jazz1016@users.noreply.github.com>
2022-05-02 22:05:27 -0600

    setup view controller, network call, and MockURLSession

--- NetworkRequest/NetworkRequest/ViewController.swift
+++ NetworkRequest/NetworkRequest/ViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
+protocol URLSessionProtocol {
+    func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol {}
+
 class ViewController: UIViewController {
     
     @IBOutlet private(set) var button: UIButton!
     private var dataTask: URLSessionDataTask?
+    var session: URLSessionProtocol = URLSession.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +34,25 @@ class ViewController: UIViewController {
               let url = URL(string: "https://itunes.apple.com/search?" + "media=ebook&term=\(encodedTerms)") else { return }
         let request = URLRequest(url: url)
         
+        dataTask = session.dataTask(with: request) {
+        
+        [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            guard let self = self else { return }
+            
+            let decoded = String(data: data ?? Data(), encoding: .utf8)
+            print("response: \(String(describing: response))")
+            print("data: \(String(describing: decoded))")
+            print("error: \(String(describing: error))")
+            
+            DispatchQueue.main.async { [ weak self] in
+                guard let self = self else { return }
+                self.dataTask = nil
+                self.button.isEnabled = true
+            }
+        }
+        button.isEnabled = false
+        dataTask?.resume()
     }
-
 
 }
 
